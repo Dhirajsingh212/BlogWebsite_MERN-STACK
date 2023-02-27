53232618f44daa3ba4afcf8f0108f4ded1962f57
Dhirajsingh212 <dhirajsingh0229@gmail.com>
2023-02-27 19:35:47 +0530

    first commit

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <>
+      <div className="footer_div" id="about">
+        <p>Copyright © 2004 - 2022 Pluralsight LLC. All rights reserved.</p>
+      </div>
+    </>
+  );
+}
